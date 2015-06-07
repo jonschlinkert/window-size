1fb5da7c26fdfffa534c21ec8f3bbb20f0152675
jonschlinkert <jon.schlinkert@sellside.com>
2015-06-07 10:27:48 -0400

    lint

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-/*
- * window-size
- * https://github.com/jonschlinkert/window-size
+/*!
+ * window-size <https://github.com/jonschlinkert/window-size>
  *
- * Copyright (c) 2014 Jon Schlinkert
+ * Copyright (c) 2014-2015 Jon Schlinkert
  * Licensed under the MIT license.
  */
 
-const tty = require('tty')
+const tty = require('tty');
 
 module.exports = (function() {
   var width;
@@ -24,10 +23,8 @@ module.exports = (function() {
       width = process.stdout.rows;
     }
   } else {
-    new Error('Error: could not get window size with tty or process.stdout');
+    new Error('window-size could not get size with tty or process.stdout.');
   }
-  return {
-    height: height,
-    width: width
-  }
+
+  return {height: height, width: width};
 })();
